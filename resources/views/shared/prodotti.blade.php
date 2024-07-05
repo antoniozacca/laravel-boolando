6c84e20c94f6f53f0dfcf1920fba66a4aac670d7
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-05 18:54:29 +0200

    add img:hover

--- resources/views/shared/prodotti.blade.php
+++ resources/views/shared/prodotti.blade.php
@@ -6,7 +6,10 @@
         @foreach ($prodotti as $item)
         <div class="col-md-4 mt-5">
             <div class="card mb-4">
-                <img src="{{ Vite::asset('resources/img/') . $item['frontImage'] }}" class="card-img-top" alt="Product Image">
+                <div class="img-container">
+                    <img src="{{ Vite::asset('resources/img/') . $item['frontImage'] }}" class="card-img-top front-img" alt="Product Image">
+                    <img src="{{ Vite::asset('resources/img/') . $item['backImage'] }}" class="card-img-top back-img" alt="Product Image">
+                </div>
                 <div class="card-body">
                     <h5 class="card-title">{{ $item['brand'] }}</h5>
                     <h2>{{ $item['name'] }}</h2>
@@ -18,3 +21,25 @@
     </div>
 </div>
 @endsection
+
+<style scoped>
+    .img-container {
+        position: relative;
+    }
+    .front-img, .back-img {
+        width: 100%;
+        transition: opacity 0.5s ease;
+    }
+    .back-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+    }
+    .img-container:hover .front-img {
+        opacity: 0;
+    }
+    .img-container:hover .back-img {
+        opacity: 1;
+    }
+</style>
